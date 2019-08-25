9913723760e198a706ee73f387e2a40d0b59386d
tmannen <teemuka90@gmail.com>
2019-08-25 09:42:28 +0300

    started augmentin pretrained fd

--- models_pose.py
+++ models_pose.py
@@ -95,3 +95,78 @@ class MobileNetSkipAddAlt(nn.Module):
             # print("{}: {}".format(i, x.size()))
         x = self.decode_conv6(x)
         return x
+
+class AugmentedFastDepth(nn.Module):
+    model = torch.load("../models/mobilenet-nnconv5dw-skipadd-pruned.pth.tar", map_location='cpu')
+    def __init__(self, model_path):
+        # TODO: mimic function above for this just use the pretrained?
+        super(AugmentedFastDepth, self).__init__()
+        self.output_size = output_size
+        mobilenet = imagenet.mobilenet.MobileNet()
+
+        for i in range(14):
+            setattr( self, 'conv{}'.format(i), mobilenet.model[i])
+
+        kernel_size = 5
+        # self.decode_conv1 = conv(1024, 512, kernel_size)
+        # self.decode_conv2 = conv(512, 256, kernel_size)
+        # self.decode_conv3 = conv(256, 128, kernel_size)
+        # self.decode_conv4 = conv(128, 64, kernel_size)
+        # self.decode_conv5 = conv(64, 32, kernel_size)
+        self.decode_conv1 = nn.Sequential(
+            depthwise(1024, kernel_size),
+            pointwise(1024, 512))
+        self.decode_conv2 = nn.Sequential(
+            depthwise(512, kernel_size),
+            pointwise(512, 256))
+        self.decode_conv3 = nn.Sequential(
+            depthwise(256, kernel_size),
+            pointwise(256, 128))
+        self.decode_conv4 = nn.Sequential(
+            depthwise(128, kernel_size),
+            pointwise(128, 64))
+        self.decode_conv5 = nn.Sequential(
+            depthwise(64, kernel_size),
+            pointwise(64, 32))
+        self.decode_conv6 = pointwise(32, 1)
+        weights_init(self.decode_conv1)
+        weights_init(self.decode_conv2)
+        weights_init(self.decode_conv3)
+        weights_init(self.decode_conv4)
+        weights_init(self.decode_conv5)
+        weights_init(self.decode_conv6)
+
+    def forward(self, x, poses):
+        # skip connections: dec4: enc1
+        # dec 3: enc2 or enc3
+        # dec 2: enc4 or enc5
+        for i in range(14):
+            layer = getattr(self, 'conv{}'.format(i))
+            x = layer(x)
+            # print("{}: {}".format(i, x.size()))
+            if i==1:
+                x1 = x
+            elif i==3:
+                x2 = x
+            elif i==5:
+                x3 = x
+
+        # Add the GP here.
+        distances = pose_distance_measure_torch(poses)
+        kernel_output = matern_kernel_torch(distances)
+        original_shape = x.shape
+        x = kernel_output.mm(x.flatten(start_dim=1)).reshape(original_shape)
+
+        for i in range(1,6):
+            layer = getattr(self, 'decode_conv{}'.format(i))
+            x = layer(x)
+            x = F.interpolate(x, scale_factor=2, mode='nearest')
+            if i==4:
+                x = x + x1
+            elif i==3:
+                x = x + x2
+            elif i==2:
+                x = x + x3
+            # print("{}: {}".format(i, x.size()))
+        x = self.decode_conv6(x)
+        return x
